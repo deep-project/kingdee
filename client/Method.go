34b71f146a55e449d02134c0fad081fb5f11d448
DESKTOP-NBQ48LN\yrj <alpiny@qq.com>
2025-08-11 13:25:12 +0800

    增加查询迭代器方法

--- client/Method.go
+++ client/Method.go
@@ -14,3 +14,23 @@ func NewMethod(c *Client, h *Handler) *Method {
 func (m *Method) IsLogin() (bool, error) {
 	return true, nil
 }
+
+// 查询迭代器
+// 可以进行翻页查询
+func (m *Method) IterateQuery(limit int, f func(startRow int) ([]map[string]any, error)) ([]map[string]any, error) {
+	var allData = []map[string]any{}
+	var page = 0
+	for {
+		page++
+		startRow := (page - 1) * limit
+		list, err := f(startRow)
+		if err != nil {
+			return allData, err
+		}
+		allData = append(allData, list...)
+		if len(list) < limit {
+			break // 最后一批
+		}
+	}
+	return allData, nil
+}
